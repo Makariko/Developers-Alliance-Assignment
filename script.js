7a715370805d489d0449bebedfd5ce7608bd21b9
Makariko <mariammargvelani66@gmail.com>
2022-09-25 19:49:06 +0300

    created script js file and done reitriving data functions in table

---
+++ script.js
@@ -0,0 +1,61 @@
+
+
+    var list1 = [];
+    var list2 = [];
+    var list3 = [];
+    var list4 = [];
+    var list5 = [];
+    var list6 = [];
+    
+    
+    
+        var n = 1;
+		var x = 0;
+
+		function AddRow(){
+
+			var AddRown = document.getElementById('show');
+			var NewRow = AddRown.insertRow(n);
+
+            
+			list1[x] = document.getElementById("name").value;
+			list2[x] = document.getElementById("last name").value;
+			list3[x] = document.getElementById("address").value;
+			list4[x] = document.getElementById("date").value;
+            list5[x] = document.getElementById("gender").value;
+            list6[x] = `<button onClick="onDelete(this)">Delete </button>`
+            
+            
+            
+            
+            var cel1 = NewRow.insertCell(0);
+			var cel2 = NewRow.insertCell(1);
+			var cel3 = NewRow.insertCell(2);
+			var cel4 = NewRow.insertCell(3);
+			var cel5 = NewRow.insertCell(4);
+            var cel6 = NewRow.insertCell(5);
+            
+            
+            
+   
+            
+
+			cel1.innerHTML = list1[x];
+            cel2.innerHTML = list2[x];
+			cel3.innerHTML = list3[x];
+			cel4.innerHTML = list4[x];
+			cel5.innerHTML = list5[x];
+            cel6.innerHTML = list6[x];
+            
+
+			n++;
+			x++;
+		}
+
+        function onDelete(td) {
+        row = td.parentElement.parentElement;
+        document.getElementById("show").deleteRow(row.rowIndex); 
+    }
+
+
+
